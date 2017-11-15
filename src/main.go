619a4fc2ad82993c742e5c6be0dbd06e31b1d804
Reid Collins <hihelloreid@gmail.com>
2017-11-14 21:52:13 -0500

    add contact page

--- src/main.go
+++ src/main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>GO, GO, GO, GOLANG!</h1>")
+	//fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>GO, GO, GO, GOLANG!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Please contact us at <a href=\"mailto:hihelloreid@gmail.com\">hihelloreid</a>.")
+	}
 }
 
 func main() {
