3a42c74350bc5ffe6ad85067b6634d6627bd1e2c
Reid Collins <hihelloreid@gmail.com>
2017-11-17 14:53:07 -0500

    DRY up form parsing by creating helper to take care of parsing

--- controllers/users.go
+++ controllers/users.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"../views"
-	"github.com/gorilla/schema"
 )
 
 type Users struct {
@@ -31,13 +30,8 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 
 //POST signup form
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, form)
