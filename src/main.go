1b13461dacf7f388dff614979cda7c835732e999
Reid Collins <hihelloreid@gmail.com>
2017-11-17 07:34:58 -0500

    add users controller with NewUsers(GET) and New(POST) to render and process signup form. specify http methods in the router.

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"../controllers"
 	"../views"
 	"github.com/gorilla/mux"
 )
@@ -10,7 +11,6 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -23,20 +23,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 func main() {
 	homeView = views.NewView("bootstrap", "../views/home.gohtml")
 	contactView = views.NewView("bootstrap", "../views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "../views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
 }
 
