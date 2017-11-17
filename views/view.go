1bb319e162ef99ba26e6436bb45c15f913a41928
Reid Collins <hihelloreid@gmail.com>
2017-11-16 20:28:51 -0500

    add Render method to View type

--- views/view.go
+++ views/view.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
 )
 
@@ -13,6 +14,11 @@ var (
 type View struct {
 	Template *template.Template //pointer to a template
 	Layout   string
+}
+
+//Render is used to render current template with passed in data
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
 
 //create new view object and parse all template files
