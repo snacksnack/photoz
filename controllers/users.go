e480a306b033f4847be4838f16f4b8f1aae571f7
Reid Collins <hihelloreid@gmail.com>
2017-11-17 13:55:36 -0500

    parse forms with gorilla schema. create SignupForm struct with schema tags

--- controllers/users.go
+++ controllers/users.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 
 	"../views"
+	"github.com/gorilla/schema"
 )
 
 type Users struct {
 	NewView *views.View
 }
 
+type SignupForm struct {
+	Email    string `schema:"email"` //struct tag for the json package
+	Password string `schema:"password"`
+}
+
+//creates Users controller
 func NewUsers() *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "../views/users/new.gohtml"),
@@ -27,7 +34,14 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, r.PostFormValue("email"))
-	fmt.Fprintln(w, r.PostFormValue("password"))
-	fmt.Fprintln(w, "Temporary placeholder - submitted form info received.")
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
+
+	//fmt.Fprintln(w, r.PostFormValue("email"))
+	//fmt.Fprintln(w, r.PostFormValue("password"))
 }
