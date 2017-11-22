c16da1d2fb1adfd7adbfae155d4ace5378e93211
Reid Collins <hihelloreid@gmail.com>
2017-11-22 10:51:24 -0500

    create UserService for interacting with User model.

--- exp/main.go
+++ exp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"../models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -33,41 +34,15 @@ type Order struct {
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
-
-	var u User
-	if err := db.Preload("Orders").First(&u).Error; err != nil {
-		panic(err)
-	}
-	fmt.Println(u)
-	fmt.Println(len(u.Orders))
-	//SELECT * FROM "orders"  WHERE "orders"."deleted_at" IS NULL AND (("user_id" IN('2'))) ORDER BY "orders"."id" ASC
-
-	var users []User
-	if err := db.Preload("Orders").Find(&users).Error; err != nil {
-		panic(err)
-	}
-	fmt.Println(users)
-	//SELECT * FROM "orders"  WHERE "orders"."deleted_at" IS NULL AND (("user_id" IN ('2','4','5','6','7')))
-
-	//createOrder(db, u, 1001, "pants1")
-	//createOrder(db, u, 1002, "pants2")
-	//createOrder(db, u, 1003, "pants3")
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	err := db.Create(&Order{
-		UserId:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	}).Error
+	defer us.Close()
+	//us.DestructiveReset()
+	user, err := us.ById(2)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(user)
 }
--- models/users.go
+++ models/users.go
@@ -1,12 +1,59 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	// ErrNotFound returned when resource cannot be found in the database
+	ErrNotFound = errors.New("models: resource not found.")
 )
 
 type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+}
+
+// look up user by id.
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+func (us *UserService) ById(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+// closes the UserService database connection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+// ONLY FOR TEST ENVIRONMENT! drop/create user table
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
 }
