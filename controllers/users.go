b6f36f1d486e2714e1d0bd2dae2768685deccd44
Reid Collins <hihelloreid@gmail.com>
2017-11-26 22:02:49 -0500

    set cookie on login

--- controllers/users.go
+++ controllers/users.go
@@ -69,15 +69,22 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	user, err := u.us.Authenticate(form.Email, form.Password)
-	switch err {
-	case models.ErrNotFound:
-		fmt.Fprint(w, "Invalid email address.")
-	case models.ErrInvalidPassword:
-		fmt.Fprint(w, "Invalid password provided")
-	case nil:
-		fmt.Fprint(w, user)
-	default:
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
+	if err != nil {
+		switch err {
+		case models.ErrNotFound:
+			fmt.Fprint(w, "Invalid email address.")
+		case models.ErrInvalidPassword:
+			fmt.Fprint(w, "Invalid password provided")
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
 	}
+
+	cookie := http.Cookie{
+		Name:  "email",
+		Value: user.Email,
+	}
+	http.SetCookie(w, &cookie)
+	fmt.Fprint(w, user)
 }
