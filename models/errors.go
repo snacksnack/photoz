b03837db4898d504661ea86df7b0add2829a1fb1
Reid Collins <hihelloreid@gmail.com>
2017-12-01 16:20:23 -0500

    add title and userid validators for gallery Create.

--- models/errors.go
+++ models/errors.go
@@ -6,9 +6,6 @@ const (
 	// ErrNotFound returned when resource cannot be found in the database
 	ErrNotFound modelError = "models: resource not found"
 
-	// ErrInvalidID returned when an invalid ID is provided to method like Delete
-	ErrIDInvalid modelError = "models: ID must be > 0"
-
 	// ErrPasswordIncorrect returned when incorrect password used for authentication
 	ErrPasswordIncorrect modelError = "models: incorrect password provided"
 
@@ -27,12 +24,21 @@ const (
 	// ErrPasswordRequired returned when Create attempted without submitting password
 	ErrPasswordRequired modelError = "models: password is required"
 
+	// ErrTitleRequired returned when trying to create Gallery with no title
+	ErrTitleRequired modelError = "models: title is required"
+
 	// ErrRememberTooShort returned when remember token is too short
-	ErrRememberTooShort modelError = "models: remember token must be at least 32 bytes"
+	ErrRememberTooShort privateError = "models: remember token must be at least 32 bytes"
 
 	// ErrRememberRequired returned when Create/Update attempted without a
 	// user remember token hash
-	ErrRememberRequired modelError = "models: remember token is required"
+	ErrRememberRequired privateError = "models: remember token is required"
+
+	// ErrUserIDRequired returned when trying to create Gallery without connected user id
+	ErrUserIDRequired privateError = "models: user ID is required"
+
+	// ErrInvalidID returned when an invalid ID is provided to method like Delete
+	ErrIDInvalid privateError = "models: ID must be > 0"
 )
 
 // want the above variables to really be constants, but cant make type error a constant
@@ -47,4 +53,10 @@ func (e modelError) Public() string {
 	split := strings.Split(s, " ")
 	split[0] = strings.Title(split[0])
 	return strings.Join(split, " ")
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
--- models/galleries.go
+++ models/galleries.go
@@ -45,3 +45,42 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
 }
+
+/////////////////////////////////////
+//VALIDATORS//
+/////////////////////////////////////
+
+type galleryValFunc func(*Gallery) error
+
+func runGalleryValFuncs(gallery *Gallery, fns ...galleryValFunc) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (gv *galleryValidator) Create(g *Gallery) error {
+	err := runGalleryValFuncs(g,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(g)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
