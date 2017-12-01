6c51f2f7ebe75c3071849629051ef07905b42301
Reid Collins <hihelloreid@gmail.com>
2017-12-01 07:34:46 -0500

    create the gallery model.

---
+++ models/galleries.go
@@ -0,0 +1,10 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+// Gallery is the image container resource
+type Gallery struct {
+	gorm.Model
+	UserID uint   `gorm:"not_null;index"`
+	Title  string `gorm:"not_null"`
+}
