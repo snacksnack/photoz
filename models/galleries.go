4605ec1e7b3ebf0166a446023debe7e0ac85b614
Reid Collins <hihelloreid@gmail.com>
2017-12-05 10:51:23 -0500

    implement the ByGalleryID method on ImageService

--- models/galleries.go
+++ models/galleries.go
@@ -5,8 +5,9 @@ import "github.com/jinzhu/gorm"
 // Gallery is the image container resource
 type Gallery struct {
 	gorm.Model
-	UserID uint   `gorm:"not_null;index"`
-	Title  string `gorm:"not_null"`
+	UserID uint     `gorm:"not_null;index"`
+	Title  string   `gorm:"not_null"`
+	Images []string `gorm:"-"`
 }
 
 type GalleryService interface {
